bb9ab11b2f3169996256a053a3fd9dabe1dc22a1
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-06-29 17:23:26 -0700

    Add RTResponse docstring with some doctests

--- rt/response.py
+++ rt/response.py
@@ -12,6 +12,29 @@ log = logging.getLogger(__name__)
 
 class RTResponse:
 
+    """Wrapper around an RT response content.
+
+    The RT response content is parsed into an :class:`RTData` instance
+    with values converted to Python types. Multipart responses are
+    parsed into an :class:`RTMultipartData` instance, which is a
+    sequence of :class:`RTData`s.
+
+    Here's a basic response with no content::
+
+        >>> content = 'RT/4.0.5 200 Ok\\n\\n# Ticket 1234 created.\\n\\n'
+        >>> RTResponse(content)  # doctest: +ELLIPSIS
+        <rt.response.RTResponse object at ...>
+
+    Responses can have multiple detail lines::
+
+        >>> content = 'RT/4.0.5 200 Ok\\n\\n# Ticket 1234 created.\\n# Ticket 1234 updated.\\n\\n'
+        >>> RTResponse(content)  # doctest: +ELLIPSIS
+        <rt.response.RTResponse object at ...>
+
+    TODO: Add more tests.
+
+    """
+
     def __init__(self, content, serializer=None, multipart=False):
         self.content = content
         self.lines = lines = content_to_lines(content)
--- rt/tests.py
+++ rt/tests.py
@@ -1,10 +1,11 @@
 import doctest
 
-from . import data
+from . import data, response
 
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(data))
+    tests.addTests(doctest.DocTestSuite(response))
     return tests
 
 
